3dd7b9d6acd45a632a500bad96a97a12951a3384
Hellings <hellingsmakondetsa@.com>
2024-12-03 03:04:14 +0200

    Updated base app

--- src/componets/container.js
+++ src/componets/container.js
@@ -1,39 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Container() {
 
-    const [todos, setToDos] = useState(
-        [
-            {
-                id: 1,
-                title: "Make Money"
-            },
-            {
-                id: 1,
-                title: "Make Money"
-            },
-            {
-                id: 1,
-                title: "Make Money"
-            },
-            {
-                id: 1,
-                title: "Make Money"
-            }
-        ]
-    );
+    localStorage.
 
+    const[newTitle, setNewTitle] = useState("");
+
+    function addToList(e) {
+        e.preventDefault();
+
+        const newItem = {id: Date.now(), title: newTitle}
+
+        setToDos([...todos, newItem]);
+    }
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
 
     return (
         <div className="container">
 
             <form>
-                <input type="text" placeholder="Add to list" /> <button>ADD</button>
+                <input type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} placeholder="Add to list" /> <button onClick={addToList}>ADD</button>
             </form>
 
             <ul>
                 {todos.map(todo => (
-                    <li>{todo.title}</li>
+                    <li key={todo.id}>{todo.title}</li>
                 ))}
             </ul>
         </div>
