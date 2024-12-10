4c184eaeb11afdb093e9f4abd551bad38d853909
Hellie-tenny <hellingsmakondetsa@gmail.com>
2024-12-10 19:34:20 +0200

    added a popup and the delete functionality

--- src/componets/container.js
+++ src/componets/container.js
@@ -2,26 +2,43 @@ import { useEffect, useState } from "react";
 
 export default function Container() {
 
-    const[newTitle, setNewTitle] = useState("");
-    const[todos, setTodos] = useState([]);
+    const [newTitle, setNewTitle] = useState("");
+    const [todos, setTodos] = useState([]);
+    const [targetItem, setTargetItem] = useState("");
 
     function addToList(e) {
         e.preventDefault();
 
-        const newItem = {id: Date.now(), title: newTitle, done: false}
+        const newItem = { id: Date.now(), title: newTitle, done: false }
         const updatedList = [...todos, newItem];
         setTodos(updatedList);
         localStorage.setItem('todos', JSON.stringify(updatedList));
+        setNewTitle("");
     }
 
-    function updateDone(id){
-        const updatedTodos = todos.map((todo) => todo.id === id ? {...todo, done: !todo.done} : todo);
+    function deleteTodo(id) {
+        const updatedTodos = todos.filter((todo) => todo.id !== id);
+        localStorage.setItem('todos', JSON.stringify(updatedTodos));
+        setTodos(updatedTodos);
+    }
+
+    function closePopup(){
+        document.getElementById('deleteDialog-container').classList.remove('active');
+    }
+
+    function openPopup(id){
+        document.getElementById('deleteDialog-container').classList.add('active');
+        setTargetItem(id);
+    }
+
+    function updateDone(id) {
+        const updatedTodos = todos.map((todo) => todo.id === id ? { ...todo, done: !todo.done } : todo);
         console.log(updatedTodos);
         localStorage.setItem('todos', JSON.stringify(updatedTodos));
         setTodos(updatedTodos);
     }
 
-    //just testing if my auto save is on 
+    //just testing if my auto save is on  
 
     useEffect(() => {
         if (localStorage.getItem('todos') != null) {
@@ -40,6 +57,19 @@ export default function Container() {
     return (
         <div className="container">
 
+            <div className="deleteDialog-container"  id="deleteDialog-container" onClick={closePopup}>
+                <div className="deleteDialog">
+                    Are you sure you want to delete this item?
+                    <div>
+                        <button onClick={() => deleteTodo(targetItem)}>Yes</button><button onClick={closePopup}>NO</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="menu">
+                <span className="active">To-do List</span><span>Objectives</span>
+            </div>
+
             <form>
                 <input type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} placeholder="Add to list..." /> <button onClick={addToList}>ADD</button>
             </form>
@@ -48,10 +78,10 @@ export default function Container() {
                 {todos.map(todo => (
                     <li key={todo.id} className={todo.done ? "done" : ""} >
                         <div>
-                            <input type="checkbox" checked={todo.done} onChange={() =>updateDone(todo.id)} />
+                            <input type="checkbox" checked={todo.done} onChange={() => updateDone(todo.id)} />
                             {todo.title}
                         </div>
-                        <div><i class="fa-regular fa-trash-can"></i></div>
+                        <div><i class="fa-regular fa-trash-can" onClick={() => openPopup(todo.id)}></i></div>
                     </li>
                 ))}
             </ul>
